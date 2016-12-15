d298bb8bbe3d50e2f7819aeb8b6a08b2384cb1db
Zack Smith <zack.smith@motorolasolutions.com>
2016-12-15 16:08:07 -0700

    more rustisms

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,18 @@ use clap::App;
 mod args;
 mod flip;
 
+use args::{times_arg, heads_arg, tails_arg, best_of_arg};
+use flip::{flip_coins, flip_best_of};
+
 fn main() {
 	let matches = App::new(env!("CARGO_PKG_NAME"))
 		.version(crate_version!())
 		.author(crate_authors!())
 		.about(env!("CARGO_PKG_DESCRIPTION"))
-		.arg(args::times_arg())
-		.arg(args::heads_arg())
-		.arg(args::tails_arg())
-		.arg(args::best_of_arg())
+		.arg(times_arg())
+		.arg(heads_arg())
+		.arg(tails_arg())
+		.arg(best_of_arg())
 		.get_matches();
 
 	let (n, heads, tails, best_of_opt) = (
@@ -29,10 +32,10 @@ fn main() {
 		value_t!(matches, "best_of", usize)
 	);
 
-	let counts = match best_of_opt {
-		Ok(best_of) => flip::flip_best_of(best_of),
-		_ => flip::flip_coins(n)
-	};
+	let counts = if let Ok(best_of) = best_of_opt {
+		flip_best_of(best_of)
+	} else { flip_coins(n) };
+
 	let winner = match counts {
 		(h, t) if h > t => heads,
 		(h, t) if t > h => tails,
